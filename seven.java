eb2657ba8b130a01890566a4576023e4d26a994c
ponymark <101316230@gms.tcu.edu.tw>
2015-02-28 00:59:41 +0800

    七言絕句
    印出七言絕句

---
+++ seven.java
@@ -0,0 +1,8 @@
+class seven {
+public static void main(String[] args) {
+System.out.println("<楓橋夜泊>\n");
+System.out.println("作者：張繼\n");
+System.out.println("月落烏啼霜滿天，江楓漁火對愁眠。\n");
+System.out.println("姑蘇城外寒山寺，夜半鐘聲到客船。\n");
+}
+}
