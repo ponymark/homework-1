8421576277786eaa3daec14cadece19e749ab53e
ponymark <101316230@gms.tcu.edu.tw>
2015-03-01 14:56:58 +0800

    七言絕句
    七言絕句

--- seven.java
+++ seven.java
@@ -1,8 +1,10 @@
-class seven {
-public static void main(String[] args) {
-System.out.println("《送孟浩然之廣陵》\n");
-System.out.println("作者：李白\n");
-System.out.println("故人西辭黃鶴樓，煙花三月下揚州。\n");
-System.out.println("孤帆遠影碧空盡，惟見長江天際流。\n");
-}
+class seven 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("《送孟浩然之廣陵》\n");
+		System.out.println("作者：李白\n");
+		System.out.println("故人西辭黃鶴樓，煙花三月下揚州。\n");
+		System.out.println("孤帆遠影碧空盡，惟見長江天際流。\n");
+	}
 }
